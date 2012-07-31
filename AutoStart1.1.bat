e500c004533b7b5fc96241687461f0055fd6b922
dorm_mang <ningma003@gmail.com>
2012-07-31 18:18:14 +0800

    中文乱码问题决 2012.2.24 9.36.31
    # AutoStart.bat----AutoStart1.1.bat 修复了 之前计划任务触发执行批处理后，批处理的dos窗口不能关闭 的bug

---
+++ AutoStart1.1.bat
@@ -0,0 +1,10 @@
+::create:2012年2月23日 modify:2012年2月24日
+::version 1.1
+::author mang
+::目的：开机自启动程序可以放入该批处理文件中，以前没有放的也不再调整。
+
+
+path=D:\Program Files\Mozilla Firefox\
+::用start的意思是计划任务执行完该批处理文件后能自动退出dos窗口。当然不加start你直接运行该bat也能退出dos窗口，但计划任务不能退出。
+start firefox https://doit.im/home#/time/today http://www.ccw.com.cn/
+::pause
