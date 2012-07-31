04f3f41b1e39b58ac74b680c69ebd0d6bc2c2d13
dorm_mang <ningma003@gmail.com>
2012-07-31 18:16:42 +0800

    中文乱码问题决2012.2.23 23.11.17
    # 增加了AutoStart.bat 用于开机自启动的批处理

---
+++ AutoStart.bat
@@ -0,0 +1,7 @@
+::create:2012年2月23日 modify:2012年2月23日
+::version 1.0
+::author mang
+::目的：开机自启动程序可以放入该批处理文件中，以前没有放的也不再调整。
+path=D:\Program Files\Mozilla Firefox\
+firefox https://doit.im/home#/time/today http://www.ccw.com.cn/
+::pause
