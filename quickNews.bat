14ba275a960778be5a755100d433abeb6b1eb466
lab_mang <ningma003@gmail.com>
2012-08-10 10:05:24 +0800

    quicknews 增加了雷锋网

--- quickNews.bat
+++ quickNews.bat
@@ -6,5 +6,5 @@
 
 path=D:\Program Files\Mozilla Firefox\
 ::用start的意思是计划任务执行完该批处理文件后能自动退出dos窗口。当然不加start你直接运行该bat也能退出dos窗口，但计划任务不能退出。
-start firefox  http://www.bbc.co.uk/ukchina/simp/topics/elt_todays_phrase/index.shtml http://lists.solidot.org /http://www.bbc.co.uk/zhongwen/simp/ 
+start firefox  http://www.bbc.co.uk/ukchina/simp/topics/elt_todays_phrase/index.shtml http://lists.solidot.org /http://www.bbc.co.uk/zhongwen/simp/ http://www.leiphone.com/
 pause
