4bbcdf58abead06bffacae5c0a7383aeada22c6b
dorm_mang <ningma003@gmail.com>
2012-10-11 22:04:55 +0800

    dormbackup.bat 增加了删除旧的ditto.db 启用新的ditto.db的语句

--- dormbackup.bat
+++ dormbackup.bat
@@ -1,5 +1,5 @@
 ::create:2011年7月28日01时23分11秒
-::lastmodify:21:13 2012-10-11
+::lastmodify:22:02 2012-10-11
 ::author:mang
 ::version:2.1
 ::用作drom备份的批处理文件,主要用于备份C盘相关目录　d盘program 目录下不能设置用户路径的程序的用户目录如vim
@@ -66,3 +66,13 @@ regedit /e "%regeditBackup%\[dorm][注册表备份][%name%].reg"
 rem 备份ftp设置
 @copy "D:\Program Files\FileZilla Server\FileZilla Server.xml" "%configBackupPath%\FTP\FileZilla Server[备份于dorm][%date%].xml"
 ::-----备份FTP配置文件----------------
+
+::-------------------删除ditto旧的数据库，启用新的数据库文件--------------------
+::因为ditto条目太多就比较慢，所以这里定期清理一次。
+
+@taskkill /f /t /im ditto.exe
+::先删除旧的文件
+del E:\ApplicationData\ditto\ditto.db
+::把重命名别一个文件为 ditto.db
+copy E:\ApplicationData\ditto\ditto_2.db E:\ApplicationData\ditto\ditto.db
+::-------------------删除ditto旧的数据库，启用新的数据库文件--------------------
