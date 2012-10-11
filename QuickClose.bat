ce4cc375ded4993d4793c6e45d8bbd6226e517f2
magn_lab <ningma003@gmail.com>
2012-10-11 13:53:32 +0800

    修改了不能彻底关闭坚果的bug,因为现在坚果有两个进程了

--- QuickClose.bat
+++ QuickClose.bat
@@ -1,6 +1,6 @@
-::last modify:07:53 2012-5-14
+::last modify:11:56 2012-10-5
 ::Author:mang
-::Version:1.0
+::Version:1.1
 ::Function:快速关闭相关程序
 ::bug:由于firefox一般是由AutoHotkey 触发，所以关闭AutoHotkey时也会关闭firefox,即把关闭fiefox的那行命令去掉，运行该批处理也会关闭firefox.
 
@@ -16,6 +16,7 @@ echo close AutoHotkey......
 taskkill /f /t /im AutoHotkey.exe
 echo close NutstoreClient......
 taskkill /f /t /im NutstoreClient.exe 
+taskkill /f /t /im  NutstoreWatchDog.exe
 echo close klive......
 taskkill /f /t /im klive.exe
 ::taskkill /f /t /im firefox.exe
