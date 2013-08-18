b3229f5db44333a37797661cae139ac8e640c2ea
mang-A43S <ningma003@gmail.com>
2013-08-18 13:00:18 +0800

    更新quickstart

--- QuickStart.bat
+++ QuickStart.bat
@@ -1,6 +1,6 @@
-::last modify:16:05 2012-5-13
+::last modify:2013-7-29 11:02:06
 ::Author:mang
-::Version:1.0
+::Version:2.2
 ::Function:快速打开相关程序，以后也可用于开机启动其它程序的批处理。
 
 
@@ -13,21 +13,61 @@ echo 设置path……
 echo 启动mang.ahk……
 D:
 cd Program Files\AutoHotkey
-start AutoHotkey "E:\ApplicationData\netDisk\klive\application\ahk\mang.ahk"
+start AutoHotkey "E:\ApplicationData\netDisk\Dropbox\application\ahk\mang.ahk"
 
-echo 启动Evernote.exe……
+
+::echo 启动Internet off……
+::D:
+::cd "D:\Program Files\InternetOff"
+::start InternetOff.exe
+
+
+echo 启动doit.im……
 D:
-cd "D:\Program Files\Evernote\Evernote\"
-start EvernoteTray.exe
+cd "D:\Program Files\Doit.im\bin\" 
+start Doit.im.exe
 
-echo 启动klive……
-C:
-cd "C:\Users\mang\AppData\Roaming\Kingsoft\klive\bin\"
-start klive.exe
-
-echo 启动nutstore……
+echo 启动listary……
 D:
-cd "D:\Program Files\nutstore\"
-start NutstoreClient.exe
+cd "D:\Program Files\Listary\" 
+start Listary.exe
+
+
+::echo 启动IPMsg.exe……
+::D:
+::cd "d:\smallTools\ipmsg\"
+::start IPMsg3.31.exe
+
+echo 启动viatc
+E:
+cd "e:\ApplicationData\netDisk\Dropbox\application\viatc"
+start viatc.exe
+
+echo 启动shaveyaks.mmap
+E:
+cd  "E:\ApplicationData\netDisk\Dropbox\1cur\"
+start ShaveYaks.mmap
+
+
+::echo 启动klive……
+::D:
+::cd "D:\Program Files\Kingsoft\Klive"
+::start kuaipan.exe
+
+::echo 启动Evernote.exe……
+::D:
+::cd "D:\Program Files\Evernote\Evernote\"
+::start EvernoteTray.exe
+
+
+::echo 启动nutstore……
+::D:
+::cd "D:\Program Files\nutstore\bin"
+::start NutstoreClient.exe
+
+::echo 启动Belvedere……
+::D:
+::cd "D:\Program Files\Belvedere\"
+::start Belvedere.exe
 
 ::pause
